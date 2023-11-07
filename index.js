b9d18d30fd46d6b1998265f0dde207f25cd111fb
Deepak Kumar <48873989+deepakmp444@users.noreply.github.com>
2023-11-07 21:56:01 +0530

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,8 +33,36 @@ app.delete("/delete", (req, res) => {
 
 app.get("/getcookie", (req, res) => {
   const getCookies = req.cookies["Deepak"];
-  res.status(200).json({ message: "get cookies", data:  getCookies});
+
+  res.status(200).json({ message: "get cookies", data: getCookies });
 });
+
+app.post("/jwt", (req, res) => {
+  const data = req.body
+
+  const jwtData = jwt.sign(data, "lsdnflkasdjflsjd", {
+    expiresIn:"1day"
+  });
+
+  const option = {
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    secure: true,
+    httpOnly: true,
+    sameSite: "none",
+    path: "/",
+  };
+
+  res.status(200).cookie("tnplab", jwtData, option).json({ data: jwtData });
+});
+
+
+app.post("/jwt-verify", (req, res) => {
+    const getCookies = req.cookies["tnplab"]
+    const jwtData = jwt.verify(getCookies, "lsdnflkasdjflsjd");
+    console.log('jwtData:', jwtData)
+  
+    res.status(200).json({ data: jwtData });
+  });
 
 
 app.listen(4004, () => {
